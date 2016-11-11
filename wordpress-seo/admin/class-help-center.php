29d2dda1bbc830a89e8c2a99a459e67fee167998
vivlong <vivlonglz@gmail.com>
2016-11-11 16:38:54 +0800

    wordpress-seo

--- wordpress-seo/admin/class-help-center.php
+++ wordpress-seo/admin/class-help-center.php
@@ -63,7 +63,7 @@ class WPSEO_Help_Center {
 			'<a href="https://yoast.com/wordpress/plugins/seo-premium/#utm_source=wordpress-seo-metabox&utm_medium=popup&utm_campaign=multiple-keywords">Yoast SEO Premium</a>',
 			'yoast.com' );
 
-		$premium_popup                    = new WPSEO_Premium_Popup( 'contact-support', 'h2', $popup_title, $popup_content );
+		$premium_popup                    = new WPSEO_Premium_Popup( 'contact-support', 'h3', $popup_title, $popup_content );
 		$contact_support_help_center_item = new WPSEO_Help_Center_Item(
 			'contact-support',
 			__( 'Email support', 'wordpress-seo' ),
@@ -115,6 +115,58 @@ class WPSEO_Help_Center {
 
 		$id = sprintf( 'tab-help-center-%s-%s', $this->group_name, $this->tab->get_name() );
 		?>
+		<div class="wpseo-tab-video-container">
+			<button type="button" class="wpseo-tab-video-container__handle" aria-controls="<?php echo $id ?>"
+			        aria-expanded="false">
+					<span
+						class="dashicons-before dashicons-editor-help"><?php _e( 'Help center', 'wordpress-seo' ) ?></span>
+				<span class="dashicons dashicons-arrow-down toggle__arrow"></span>
+			</button>
+			<div id="<?php echo $id ?>" class="wpseo-tab-video-slideout">
+				<div class="yoast-help-center-tabs">
+					<ul>
+						<?php
+						$class = 'active wpseo-help-center-item';
+
+						foreach ( $help_center_items as $help_center_item ) {
+							$id = $help_center_item->get_identifier();
+							$dashicon = $help_center_item->get_dashicon();
+							if ( ! empty( $dashicon ) ) {
+								$dashicon = 'dashicons-before ' . $dashicon;
+							}
+							$link_id  = "tab-link-{$this->group_name}_{$this->tab->get_name()}__{$id}";
+							$panel_id = "tab-panel-{$this->group_name}_{$this->tab->get_name()}__{$id}";
+							?>
+
+							<li id="<?php echo esc_attr( $link_id ); ?>" class="<?php echo $class; ?>">
+								<a href="<?php echo esc_url( "#$panel_id" ); ?>"
+								   class="<?php echo $id . ' ' . $dashicon?>"
+								   aria-controls="<?php echo esc_attr( $panel_id ); ?>"><?php echo esc_html( $help_center_item->get_label() ); ?></a>
+							</li>
+							<?php
+							$class = 'wpseo-help-center-item';
+						}
+						?>
+					</ul>
+				</div>
+				<div class="contextual-help-tabs-wrap">
+					<?php
+					$classes = 'help-tab-content active';
+					foreach ( $help_center_items as $help_center_item ) {
+						$id = $help_center_item->get_identifier();
+
+						$panel_id = "tab-panel-{$this->group_name}_{$this->tab->get_name()}__{$id}";
+						?>
+						<div id="<?php echo esc_attr( $panel_id ); ?>" class="<?php echo $classes; ?>">
+							<?php echo $help_center_item->get_content(); ?>
+						</div>
+						<?php
+						$classes = 'help-tab-content';
+					}
+					?>
+				</div>
+			</div>
+		</div>
 		<?php
 	}
 
@@ -126,7 +178,7 @@ class WPSEO_Help_Center {
 	 * @return bool
 	 */
 	private function is_a_help_center_item( $item ) {
-		return is_a( $item, 'WPSEO_Help_Center_Item' );
+		return ( $item instanceof WPSEO_Help_Center_Item );
 	}
 
 	/**
@@ -139,7 +191,7 @@ class WPSEO_Help_Center {
 	public static function get_translated_texts() {
 		return array(
 			/* translators: %s: '%%term_title%%' variable used in titles and meta's template that's not compatible with the given template */
-			'variable_warning' => sprintf( __( 'Warning: the variable %s cannot be used in this template.', 'wordpress-seo' ), '<code>%s</code>' ) . ' ' . __( 'See the help tab for more info.', 'wordpress-seo' ),
+			'variable_warning' => sprintf( __( 'Warning: the variable %s cannot be used in this template. See the help center for more info.', 'wordpress-seo' ), '<code>%s</code>' ),
 			'locale' => get_locale(),
 			/* translators: %d: number of knowledge base search results found. */
 			'kb_found_results' => __( 'Number of search results: %d', 'wordpress-seo' ),
