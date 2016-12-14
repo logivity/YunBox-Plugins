ed67628903109709c8a443f005dde7ac56c490c1
vivlong <vivlonglz@gmail.com>
2016-12-14 17:48:50 +0800

    rm wp-admin-ui

--- wp-admin-ui/assets/js/wpadminui-ajax-license.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery('#wpui-edd-license-btn').on('click', function() {
-		jQuery( '.spinner' ).css( "visibility", "visible" );
-		jQuery( '.spinner' ).css( "float", "left" );
-	});
-});
